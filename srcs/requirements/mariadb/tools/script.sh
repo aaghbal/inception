2cb686b2e2f44a4805c8f85db57d0e2ba5a87e16
aaghbal <abdellahaghbal931@gmail.cm>
2024-07-24 22:18:40 +0100

    edit: fix mariadb error

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,16 +1,12 @@
-
-DB_NAME="dd"
-DB_USER="abdo"
-DB_PASS="1234"
-
+#!/bin/bash
 
 service mariadb start
 
-mysql -u root <<-delm
-    CREATE DATABASE IF NOT EXISTS ${DB_NAME};
-    CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
-    GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
-    FLUSH PRIVILEGES;
-delm
-
+mysql -u root <<-DEL
+        CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;
+        CREATE USER IF NOT EXISTS '$DATABASE_USER'@'%' IDENTIFIED BY '$DATABASE_PASS';
+        GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$DATABASE_USER'@'%';
+        FLUSH PRIVILEGES;
+DEL
 service mariadb stop
+exec mysqld_safe --bind-address=0.0.0.0
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -4,18 +4,14 @@ chmod +x wp-cli.phar
 
 mv wp-cli.phar /usr/local/bin/wp
 
-mkdir /var/www/html/wp-cli
+mkdir /var/www/wordpress
 
-cd /var/www/html/wp-cli
-
+cd /var/www/wordpress
 wp core download --allow-root
 
-cp wp-config-sample.php wp-config.php
 
-wp config set DB_HOST mariadb --allow-root
-wp config set DB_NAME dd --allow-root
-wp config set DB_USER abdo --allow-root
-wp config set DB_PASSWORD 1234 --allow-root
 
+wp config create  --dbname=wp_database --dbuser=aaghbal --dbhost=mariadb --dbpass=1234 --allow-root
 wp core install --allow-root --url=aaghbal.42.fr --title=my_inception --admin_user=wp_abdo   --admin_email=abdo@example.com 
 
+tail -f /dev/null
